78abbe16debf92aa8fe123b4efc8751a05ef8df7
Josen-B <baiqiaosen@gmail.com>
2026-08-03 03:13:37 +0000

    fix(axvm): wrap hypercall return value in HyperCallOutcome enum

--- virtualization/axvm/src/runtime/hvc.rs
+++ virtualization/axvm/src/runtime/hvc.rs
@@ -684,7 +684,7 @@ impl HyperCall {
                     notify_irq
                 );
 
-                Ok(0)
+                Ok(HyperCallOutcome::Return(0))
             }
             _ => {
                 warn!("Unsupported hypercall code: {:?}", self.code);
